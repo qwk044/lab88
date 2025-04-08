97053210e5cc2b0556675c9da5d2561c34aef21e
qwk044 <mr.erko04@gmail.com>
2025-04-08 11:49:54 +0500

    change MainActivity

--- app/src/main/java/com/example/lab88/MainActivity.kt
+++ app/src/main/java/com/example/lab88/MainActivity.kt
@@ -1,21 +1,28 @@
-package com.example.lab88
+package com.example.lab8
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var reminderIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        reminderIntent = Intent(this, ReminderService::class.java)
     }
 
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_start_reminder -> startService(reminderIntent)
+            R.id.button_stop_reminder -> stopService(reminderIntent)
+            R.id.button_next_activity -> {
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 }
