5c17080c0106856bf6b04ecfeef1c5b513797fe7
qwk044 <mr.erko04@gmail.com>
2025-04-08 11:54:56 +0500

    change activity2

--- app/src/main/java/com/example/lab88/MainActivity2.kt
+++ app/src/main/java/com/example/lab88/MainActivity2.kt
@@ -1,4 +1,25 @@
-package com.example.lab88
+package com.example.lab8
 
-class MainActivity2 {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var musicServiceIntent: Intent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        musicServiceIntent = Intent(this, MusicForegroundService::class.java)
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_music_start -> ContextCompat.startForegroundService(this, musicServiceIntent)
+            R.id.button_music_stop -> stopService(musicServiceIntent)
+        }
+    }
 }
